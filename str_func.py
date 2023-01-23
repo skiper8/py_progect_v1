d459379babedc29a2d58fda7a6eccf3cdba0a9b5
skiper8 <123262347+skiper8@users.noreply.github.com>
2023-01-23 07:27:01 +0300

    Update str_func.py

--- str_func.py
+++ str_func.py
@@ -1,2 +1,8 @@
 def str_upper(word):
+    """Дедает все буквы слова заглавными"""
     return word.upper()
+
+
+def str_upper_1(word):
+    """Делайт заглавной первую букву слова"""
+    return word[1].upper()
